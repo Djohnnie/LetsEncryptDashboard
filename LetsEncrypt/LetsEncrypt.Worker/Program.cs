8766210b8fb8fba02c5c3908ad3c6615e5ec43b9
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2024-03-20 16:47:05 +0100

    Updated worker time to collect averages

--- LetsEncrypt/LetsEncrypt.Worker/Program.cs
+++ LetsEncrypt/LetsEncrypt.Worker/Program.cs
@@ -1,7 +1,9 @@
 using LetsEncrypt.Worker;
 using LetsEncrypt.Managers.DependencyInjection;
+using LetsEncrypt.Worker.Processing;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddScoped<CertificateProcessor>();
 builder.Services.AddHostedService<Worker>();
 builder.Services.AddManagers();
 
